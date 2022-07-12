a44eb9c3795c7ac83d638667d3099792a992781d
sariyanta <desar@sariyanta.com>
2022-07-12 20:08:36 +0200

    Test app

---
+++ app/app/tests.py
@@ -0,0 +1,31 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """
+    Test the calc module
+    """
+
+    def test_add_numbers(self):
+        """
+        Test adding numbers together
+        """
+
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """
+        Test substracting numbers
+        """
+
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, 5)
